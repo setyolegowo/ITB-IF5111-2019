463c827d33eddc77f8534330f607f8d43e2662c4
Setyo Legowo <setyolegowo@users.noreply.github.com>
2019-02-23 17:09:17 +0700

    Add time record statement

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,21 @@
 
 #include <iostream>
 #include <fstream>
+#include <sys/time.h>
 
+#include "algo/base_algo.hpp"
 #include "algo/binary_search_tree.hpp"
 #include "node_structure.hpp"
+
+/**
+ * Returns the current time in microseconds.
+ */
+long getMicrotime()
+{
+    struct timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+    return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
+}
 
 void readFile(char* filename, std::vector<House>* houseList)
 {
@@ -24,23 +36,41 @@ void readFile(char* filename, std::vector<House>* houseList)
     csvfile.close();
 }
 
+BaseAlgorithm * chooseAlgorithm(char* choose, int argc, char** argv)
+{
+    if (strcmp(choose, "binary") == 0) {
+        std::cout << "Using binary search" << std::endl;
+        return new BinarySearch();
+    }
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
-    if (argc == 1) {
-        std::cerr << "Lack of arguments: " << argv[0] << " <datafile>" << std::endl;
-        return 0;
+    if (argc < 3) {
+        std::cerr << "Lack of arguments: " << argv[0] << " <datafile> <algorithm>" << std::endl;
+        return -127;
     }
 
+    BaseAlgorithm * algo = chooseAlgorithm(argv[2], argc, argv);
+    if (algo == 0) {
+        std::cerr << "The algorithm is not found" << std::endl;
+        return -127;
+    }
+
+    long start_time, finish_time;
     std::vector<House> houseList;
     readFile(argv[1], &houseList);
 
-    std::cout << "Using binary search" << std::endl;
-    BinarySearch search;
-    if (search.find(&houseList)) {
+    start_time = getMicrotime();
+    if (algo->find(&houseList)) {
         std::cout << "FOUND" << std::endl;
     } else {
         std::cout << "NOT FOUND" << std::endl;
     }
+    finish_time = getMicrotime();
+
+    std::cout << (finish_time - start_time) << " microseconds" << std::endl;
 
     return 0;
 }
