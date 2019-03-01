af3d1f32f4f7f91fe56a9c329a3acb3c9fc5dd18
Setyo Legowo <setyolegowo@users.noreply.github.com>
2019-03-01 08:40:16 +0700

    Fix test data source

--- test_scripts/square.sh
+++ test_scripts/square.sh
@@ -3,7 +3,7 @@
 declare -a result
 for n in {100000..1000000..100000}; do
     for i in {1..20}; do
-        result[$i]=$(dist/output data/daftar_rumah.small.csv $n square average | grep -Eo "[0-9]+")
+        result[$i]=$(dist/output data/daftar_rumah.medium.csv $n square average | grep -Eo "[0-9]+")
     done
     combine_result=`echo ${result[@]} | sed 's/ /;/g'`
     echo "\"R $n\";$combine_result"
